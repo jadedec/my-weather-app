0d6ff2cb8d058d83019f2437c567b3a6ebf06895
jadedec <106964384+jadedec@users.noreply.github.com>
2022-09-28 13:42:29 +0100

    current weather and forecast

---
+++ src/api.js
@@ -0,0 +1,13 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '18b327a222msh8549cce9aa5d022p1a947cjsnb42b4acfb005',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5/";
+
+export const WEATHER_API_KEY="c925fa63e8197551d05ee2ead2add239"
