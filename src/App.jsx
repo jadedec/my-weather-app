5731389657887f172ffe26107851d14090f00414
jadedec <106964384+jadedec@users.noreply.github.com>
2022-09-28 14:18:26 +0100

    try to get realtime location

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Search from "./components/search/search";
 import CurrentWeather from "./components/current-weather/current-weather";
 import Forecast from "./components/forecast/forecast";
@@ -6,12 +6,21 @@ import { WEATHER_API_URL, WEATHER_API_KEY } from "./api";
 import "./App.css";
 
 function App() {
+  // const [lat, setLat] = useState([]);
+  // const [lon, setLon] = useState([]);
   const [currentWeather, setCurrentWeather] = useState(null);
   const [forecast, setForecast] = useState(null);
 
-  const handleOnSearchChange = (searchData) => {
-    const [lat, lon] = searchData.value.split(" ");
+  // const getLocation = async () => {
+  //   navigator.geolocation.getCurrentPosition(function(position) {
+  //     setLat(position.coords.latitude);
+  //     setLon(position.coords.longitude);
+  //   });
+  //   await fetchData();
+  // }
 
+  const handleOnSearchChange = (searchData) => {
+    const [lat,lon] = searchData.value.split(" ");
     const currentWeatherFetch = fetch(
       //units=metrics
       `${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`
@@ -31,6 +40,32 @@ function App() {
       .catch((err)=>console.log(err));
   };
 
+  // const fetchData = () => {
+  //   const currentWeatherFetch = fetch(
+  //     //units=metrics
+  //     `${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`
+  //   );
+  //   const forecastFetch = fetch(
+  //     `${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`
+  //   );
+
+  //   Promise.all([currentWeatherFetch, forecastFetch])
+  //     .then(async (response) => {
+  //       const weatherResponse = await response[0].json();
+  //       const forecastResponse = await response[1].json();
+
+  //       setCurrentWeather({weatherResponse });
+  //       setForecast({forecastResponse });
+
+        
+  //     })
+  //     .catch((err)=>console.log(err));
+  // }
+
+  // useEffect(() => {
+  //   getLocation();
+  // }, );
+  
   console.log(currentWeather);
   console.log(forecast);
   
